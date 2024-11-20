0e7d55b7f700e98efd633ec6d0a88febe4d5d6be
Daniel <13357734+thedch@users.noreply.github.com>
2024-11-19 21:08:19 -0800

    HTTPS let's goooooo

--- src/content/projects/vlm-fun/VLMTester.tsx
+++ src/content/projects/vlm-fun/VLMTester.tsx
@@ -117,7 +117,7 @@ const VideoVLMComponent = () => {
           formData.append('file', blob, 'webcam.jpg');
 
           console.log('Sending frame to API');
-          const response = await fetch('http://192.34.62.96:8000/predict/', {
+          const response = await fetch('https://predict.dch.xyz/predict/', {
             method: 'POST',
             body: formData,
           });
